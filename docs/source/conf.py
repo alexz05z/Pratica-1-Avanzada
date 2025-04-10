bb897fca647fa7dbeeda40ae64186abb07733223
alex <alexfraleda@gmail.com>
2025-04-10 15:12:37 +0200

    todo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,9 +16,7 @@ release = '1'
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'sphinx_rtd_theme',
     ]
 
 templates_path = ['_templates']
